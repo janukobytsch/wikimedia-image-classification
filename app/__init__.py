3a63d92b7c564cdc3f67077897cd2702b47c1919
Janusch Jacoby <janusch.jacoby@student.hpi.uni-potsdam.de>
2015-08-30 13:18:22 +0200

    Add celery message queue

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
+from celery import Celery
 
 app = Flask(__name__)
+
 app.config.from_object('app.config')
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
 CsrfProtect(app)
 
 from app import views
